b6a3e098c504d08eb6860e641e8201988c15a580
Tahmid Zaman <tahmidzaman@gmail.com>
2018-05-13 20:17:57 +0100

    0.4.1: Removed experimental script and replaced GUI code. Superfluous code removed from main script.

--- Main/GUI.py
+++ Main/GUI.py
@@ -7,152 +7,106 @@
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
+from kivy.lang import Builder
 from kivy.core.audio import SoundLoader
-from kivy.uix.slider import Slider
-import threading
 
-sm = None
 position = 0
-tbSlider = None
-isSliderTracking = False
-shouldSliderTrack = False
-
-song = SoundLoader.load("DoIt.mp3")
 
 
-class SliderThread(threading.Thread):
-    def run(self):
-        UpdateSlider()
-
-
-def PlayPauseSong(instance):
-    global position, isSliderTracking, shouldSliderTrack
-    if song.state == "stop":
-        song.play()
-        song.seek(position)
-        instance.text = "Pause"
-        shouldSliderTrack = True
-    elif song.state == "play":
-        print(position)
-        shouldSliderTrack = False
-        song.stop()
-        instance.text = "Play"
-
-
-def CreateToolbarButtons():
-    # anchored toolbar buttons to centre of toolbar
-    toolbarButtonsAnchor = AnchorLayout(anchor_x="center", anchor_y="center")
-
-    # created gridlayout for 5 toolbar buttons
-    toolbarButtons = GridLayout(size_hint=(0.5, 0.5))
-    toolbarButtons.cols = 5
-    toolbarButtonsAnchor.add_widget(toolbarButtons)
-
-    # Creates the toolbar buttons
-    shuffleBtn = Button(text="Shuffle")
-    backBtn = Button(text="Back")
-    playBtn = Button(text="Play")
-    playBtn.bind(on_press=PlayPauseSong)
-
-    forwardBtn = Button(text="Forward")
-    loopBtn = Button(text="Loop")
-
-    # adding buttons to the toolbar
-    toolbarButtons.add_widget(shuffleBtn)
-    toolbarButtons.add_widget(backBtn)
-    toolbarButtons.add_widget(playBtn)
-    toolbarButtons.add_widget(forwardBtn)
-    toolbarButtons.add_widget(loopBtn)
-
-    return toolbarButtonsAnchor
-
-
-def CreateScreenManager():
-    global sm
-    sm = ScreenManager(size_hint=(1, 4))
-    screen = Screen(name='Page 1')
-    sm.add_widget(screen)
-    screen.add_widget(Button(text="Main Page"))
-    screen2 = Screen(name="Page 2")
-    sm.add_widget(screen2)
-    screen2.add_widget(Button(text="Playlist Page"))
-
-    return sm
-
-
-def screenChange(instance): # functionality for "switch" button
-    if sm.current == "Page 1":
-        sm.current = "Page 2"
-    elif sm.current == "Page 2":
-        sm.current = "Page 1"
-
-
-def CreateSwitchButton():
-    # anchored switch button to right of toolbar
-    sbAnchor = AnchorLayout(anchor_x="right", anchor_y="center")
-
-    # Creates switch button
-    changeButton = Button(size_hint = (0.125, 0.5), text="Switch")
-    changeButton.bind(on_press=screenChange)
-    sbAnchor.add_widget(changeButton)
-
-    return sbAnchor
-
-
-def CreateSlider():
-    global tbSlider
-
-    sliderAnchor = AnchorLayout(anchor_x="center", anchor_y="bottom")
-    tbSlider = Slider(min=0, max=song.length, size_hint = (0.6, 0.3))
-    sliderAnchor.add_widget(tbSlider)
-
-    mySliderThread = SliderThread()
-    mySliderThread.start()
-    return sliderAnchor
-
-
-def UpdateSlider():
-    global position, shouldSliderTrack, isSliderTracking
-    while True:
-        if isSliderTracking == True and shouldSliderTrack == False:
-            isSliderTracking = False
-        if shouldSliderTrack == True:
-            position = song.get_pos()
-        tbSlider.value = position
-
-
-class BaseGUI(GridLayout): # root widget for entire GUI
-
+class BaseGUI(GridLayout):
     def __init__(self, **kwargs):
         super(BaseGUI, self).__init__(**kwargs)
-        self.rows = 2
 
-        self.add_widget(CreateScreenManager())
+    song = SoundLoader.load("DoIt.mp3")
 
-        toolbar = FloatLayout()
-        self.add_widget(toolbar)
+    def ScreenChange(self):
+        myScreenManager = self.ids["sm"]
+        if myScreenManager.current == "Main":
+            myScreenManager.current = "Playlist"
+        elif myScreenManager.current == "Playlist":
+            myScreenManager.current = "Main"
 
-        toolbar.add_widget(CreateSwitchButton())
-        toolbar.add_widget(CreateToolbarButtons())
-        toolbar.add_widget(CreateSlider())
+    def PlayPauseSong(self):
+        global position
+        playButton = self.ids["Playbtn"]
+        if self.song.state == "stop":
+            self.song.play()
+            self.song.seek(position)
+            print("play " + str(position))
+            playButton.text = "Pause"
+        elif self.song.state == "play":
+            position = self.song.get_pos()
+            print("stop " + str(position))
+            self.song.stop()
+            playButton.text = "Play"
+
+    def Skip(self):
+        global position
+        mySlider = self.ids["slider"]
+        self.song.seek(mySlider.value)
+        position = mySlider.value
+        print("skipped to " + str(position))
 
 
-class MyApp(App):
+root_widget = Builder.load_string("""
 
+BaseGUI: 
+    rows: 2
+    ScreenManager:
+        id: sm
+        size_hint: (1,4)
+        Screen:
+            name: "Main"
+            Button:
+                text: "Main Page"
+        Screen:
+            name: "Playlist"
+            Button:
+                text: "Playlist Page"
+    FloatLayout:
+        id: toolbar
+        AnchorLayout:
+            anchor_x: "right"
+            anchor_y: "center"
+            Button:
+                size_hint: (0.125, 0.5)
+                text: "Switch"
+                on_release: root.ScreenChange()
+        AnchorLayout:
+            anchor_x: "center"
+            anchor_y: "center"
+            GridLayout:
+                size_hint: (0.5, 0.5)
+                cols: 5
+                Button:
+                    text: "Shuffle"
+                Button:
+                    text: "Back"
+                Button:
+                    id: Playbtn
+                    text: "Play"
+                    on_release: root.PlayPauseSong()
+                Button:
+                    text: "Forward"
+                Button:
+                    text: "Loop"
+        AnchorLayout:
+            anchor_x: "center"
+            anchor_y: "bottom"
+            Slider:
+                id: slider
+                min: 0
+                max: root.song.length
+                size_hint: (0.6, 0.3)
+                on_touch_up: root.skip()
+
+
+""")
+
+
+class ExperimentalApp(App):
     def build(self):
-        return BaseGUI()  # Build according to the BaseGUI.
+        return root_widget  # Build according to the BaseGUI.
 
 
-def DisplayGUI():
-    if __name__ == '__main__':
-        MyApp().run()
-
-
-DisplayGUI()
+ExperimentalApp().run()
